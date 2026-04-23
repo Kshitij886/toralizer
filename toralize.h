cf9197c6d368ac9cc1e9c75921fb4ca5f37cd33e
Kshitij886 <kshitijkhatri437@gmail.com>
2026-04-23 10:23:04 +0545

    feat: makefile for running the c file, and made the response from socks5

--- toralize.h
+++ toralize.h
@@ -1,29 +1,40 @@
-/* toralize.h */
-#include "stdio.h"
-#include "stdlib.h"
-#include "string.h"
-#include <unistd.h>
+#ifndef TORALIZE_H
+#define TORALIZE_H
 
-#define PROXY       "127.0.0.1"
-#define PROXYPORT    9050
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#define PROXY "127.0.0.1"
+#define PROXYPORT 9050
 
 typedef unsigned char int8;
 typedef unsigned short int int16;
 typedef unsigned int int32;
 
-/*
-             +----+----+----+----+----+----+----+----+----+----+....+----+
-             | VN | CD | DSTPORT |      DSTIP        | USERID       |NULL|
-             +----+----+----+----+----+----+----+----+----+----+....+----+
-# of bytes	   1    1      2              4           variable       1
-
-*/
-
-/* Struct for this packet */
-struct proxy_request{
+/* SOCKS5 request */
+struct proxy_request {
     int8 vn;
     int8 cd;
     int16 dstport;
     int32 dstip;
     unsigned char userid[8];
 };
+
+typedef struct proxy_request Req;
+
+/* SOCKS5 response */
+struct proxy_response {
+    int8 vn;
+    int8 cd;
+    int16 _;
+    int32 __;
+};
+
+typedef struct proxy_response Res;
+
+#endif
