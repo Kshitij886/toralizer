bff3083bac2ae39c908d82d09ecc1c4834f53c75
Kshitij886 <kshitijkhatri437@gmail.com>
2026-04-23 09:51:41 +0545

    created a proxy request struct

--- toralize.h
+++ toralize.h
@@ -6,3 +6,24 @@
 
 #define PROXY       "127.0.0.1"
 #define PROXYPORT    9050
+
+typedef unsigned char int8;
+typedef unsigned short int int16;
+typedef unsigned int int32;
+
+/*
+             +----+----+----+----+----+----+----+----+----+----+....+----+
+             | VN | CD | DSTPORT |      DSTIP        | USERID       |NULL|
+             +----+----+----+----+----+----+----+----+----+----+....+----+
+# of bytes	   1    1      2              4           variable       1
+
+*/
+
+/* Struct for this packet */
+struct proxy_request{
+    int8 vn;
+    int8 cd;
+    int16 dstport;
+    int32 dstip;
+    unsigned char userid[8];
+};
